dd56f5de82ce7e41a87cac7086dbb1404a0de6de
Janani NA <janani2285@gmail.com>
2020-07-03 21:19:17 -0700

    render text

--- generator.js
+++ generator.js
@@ -10,9 +10,12 @@ License
 Contributing
 Tests
 Questions */
-getUserInput();
-function getUserInput(){
-    inquirer.prompt([
+ getUserInput();
+//console.log(renderContent(ans));
+//writeToFile();
+
+ async function getUserInput(){
+    const answers = await inquirer.prompt([
         {
             type: "input",
             message: "Enter your github username:",
@@ -45,7 +48,7 @@ function getUserInput(){
           },
           
         {
-          type: "checkbox",
+          type: "list",
           message: "Choose from below the list of license you like to include to your project",
           name:"license",
           choices: [
@@ -55,7 +58,59 @@ function getUserInput(){
             "Public Domain Dedication and License (PDDL)",
           ]
         }
-      ]).then(function(answers) {
-        console.log(answers);
-      });
+      ]);
+      
+        console.log(renderContent(answers));
+    
+}
+
+function renderContent(answer){
+   // const answer = getUserInput();
+    return `# ${answer.title}
+
+    ## Badges
+    
+    ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+    
+    Badges aren't _necessary_, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+   
+    ## Description 
+    
+   ${answer.description}
+    
+    
+    ## Table of Contents 
+    
+   
+    
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [Credits](#credits)
+    * [License](#license)
+    * [Questions](#questions)
+    
+    ## Installation
+    
+   ${answer.installation}
+    
+    
+    ## Usage 
+    
+   ${answer.usage}
+    
+     
+    ## License
+    
+    The last section of a good README is a license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, use [https://choosealicense.com/](https://choosealicense.com/)
+    
+    
+    
+    
+    ## Questions
+    
+    Visit https://github.com/${answer.github} (github repo) for the code
+
+    Reach me @ ${answer.email} for further questions
+    
+    `
 }
